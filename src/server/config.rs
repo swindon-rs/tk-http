6de531ab24e9e1a6d3f2f4f521338cce91466f1b
Paul Colomiets <paul@colomiets.name>
2017-02-13 17:52:18 +0200

    Accessor functions for timeout config

--- src/server/config.rs
+++ src/server/config.rs
@@ -36,4 +36,49 @@ impl Config {
     pub fn done(&mut self) -> Arc<Config> {
         Arc::new(self.clone())
     }
+    /// Timeout receiving very first byte over connection
+    pub fn first_byte_timeout(&mut self, value: Duration) -> &mut Self {
+        self.first_byte_timeout = value;
+        self
+    }
+    /// Timeout of idle connection (when no request has been sent yet)
+    pub fn keep_alive_timeout(&mut self, value: Duration) -> &mut Self {
+        self.keep_alive_timeout = value;
+        self
+    }
+    /// Timeout of receiving whole request headers
+    ///
+    /// This timeout starts when first byte of headers is received
+    pub fn headers_timeout(&mut self, value: Duration) -> &mut Self {
+        self.headers_timeout = value;
+        self
+    }
+    /// Maximum delay between any two bytes of input request received
+    pub fn input_body_byte_timeout(&mut self, value: Duration) -> &mut Self {
+        self.input_body_byte_timeout = value;
+        self
+    }
+    /// Timeout of whole request body received
+    ///
+    /// This timeout might be adjusted on per-request basis in
+    /// `headers_received`.
+    pub fn input_body_whole_timeout(&mut self, value: Duration) -> &mut Self {
+        self.input_body_whole_timeout = value;
+        self
+    }
+    /// Maximum delay between any two bytes of the output request could be
+    /// sent
+    pub fn output_body_byte_timeout(&mut self, value: Duration) -> &mut Self {
+        self.output_body_byte_timeout = value;
+        self
+    }
+    /// Timeout for the whole response body to be send to the client
+    ///
+    /// This timeout is taken literally for any response, so it must be
+    /// as large as needed for slowest client fetching slowest file. I.e.
+    /// it's fine if it's
+    pub fn output_body_whole_timeout(&mut self, value: Duration) -> &mut Self {
+        self.output_body_whole_timeout = value;
+        self
+    }
 }
